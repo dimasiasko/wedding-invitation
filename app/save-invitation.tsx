632e93cdbae446177c285e7c6aa1995305093348
Dmytro Siasko <70683494+dimasiasko@users.noreply.github.com>
2026-07-28 20:06:49 +0300

    Update save-invitation.tsx

--- app/save-invitation.tsx
+++ app/save-invitation.tsx
@@ -5,7 +5,7 @@ const basePath = process.env.NODE_ENV === "production" ? "/wedding-invitation" :
     return (
         <a target="_blank"
             download="invitation.png"
-            href=`${basePath}/assets/invitation.png`
+            href={`${basePath}/assets/invitation.png`}
             className="group relative block max-w-[300px] mx-auto overflow-hidden border border-[var(--minimal-title)]/40 hover:border-[var(--minimal-title)] transition-colors duration-500">
             <span className="relative z-10 font-bold block max-w-[300px] mx-auto text-[var(--minimal-title)] px-10 py-3.5 text-[10px] uppercase tracking-[0.2em] transition-all duration-500 group-hover:text-white text-center">
                 Збережіть запрошення
